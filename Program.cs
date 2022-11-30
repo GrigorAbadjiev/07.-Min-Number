254852be7cec1e8e07a97697b315f33d736e91eb
User <User@DESKTOP-ESP2JT1>
2022-11-30 17:27:22 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _07._Min_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            double minNumber = double.MaxValue;
+
+            while (input != "Stop")
+            {
+                double inputAsNumber = double.Parse(input);
+
+                if (inputAsNumber < minNumber)
+                {
+                    minNumber = inputAsNumber;
+                }
+                input = Console.ReadLine();
+            }
+            Console.WriteLine(minNumber);
+        }
+    }
+}
